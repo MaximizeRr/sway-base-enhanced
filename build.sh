38d7343fb30eba4d6cf66d34475c25e41de4c73b
Maxime Roger <maxime.roger@cleverconnect.com>
2024-12-31 04:28:18 +0100

    feat: Theme selector working, better waybar conf

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
-sudo bluebuild build --tempdir /mnt/samba/tmp --build-driver buildah -c zstd --rechunk-clear-plan recipes/base.yml && \
+BUILD_OPTS="--rechunk --rechunk-clear-plan"
+BUILD_OPTS=""
+sudo bluebuild build --tempdir /mnt/samba/tmp --build-driver buildah -c zstd ${BUILD_OPTS} recipes/base.yml && \
   sudo buildah tag localhost/sway-aura:latest git.internal.myceliu.me/azure/sway-aura:latest && \ 
   sudo buildah push git.internal.myceliu.me/azure/sway-aura:latest
